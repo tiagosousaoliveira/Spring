1d1d209a59bf42ef52f1e5a7e6a1476c4d89b01c
tiagosousaoliveira <tiagossousa61@gmail.com>
2018-06-14 21:50:56 -0300

    Inicialização de Objetos no metodo main

--- src/main/java/com/tiago/cursomc/CursomcApplication.java
+++ src/main/java/com/tiago/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.tiago.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.tiago.cursomc.Domain.Categoria;
+import com.tiago.cursomc.repositories.CategoriaRepositorio;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepositorio categoriaRepositorio;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepositorio.save(Arrays.asList(cat1,cat2));
+		
 	}
 }
